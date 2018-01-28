77f3e2af3f8907cf3a89fd44bb8420f31b370676
Quentin <quentin.tournier2@gmail.com>
2018-01-28 13:31:08 +0100

    add cors support

--- CinemaServer/src/main/java/manager/CategorieRepository.java
+++ CinemaServer/src/main/java/manager/CategorieRepository.java
@@ -2,10 +2,11 @@ package manager;
 
 import model.Categorie;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by Quentin on 03/11/2017.
  */
-
+@CrossOrigin
 public interface CategorieRepository extends CrudRepository <Categorie, Integer> {
 }
--- CinemaServer/src/main/java/manager/PersonnageRepository.java
+++ CinemaServer/src/main/java/manager/PersonnageRepository.java
@@ -2,10 +2,11 @@ package manager;
 
 import model.Personnage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by Quentin on 03/11/2017.
  */
-
+@CrossOrigin
 public interface PersonnageRepository extends CrudRepository <Personnage, Integer> {
 }
