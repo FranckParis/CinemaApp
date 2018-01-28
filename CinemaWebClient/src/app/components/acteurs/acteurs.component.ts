9f54fca1020d86096a1a91d5bb9cd84e99742b08
Menros <gondras.plouis@gmail.com>
2018-01-28 23:38:46 +0100

    Add acteur works !

--- CinemaWebClient/src/app/components/acteurs/acteurs.component.ts
+++ CinemaWebClient/src/app/components/acteurs/acteurs.component.ts
@@ -11,26 +11,71 @@ export class ActeursComponent implements OnInit {
 
   acteurs: Acteur[] = [];
   acteur: Acteur;
+  validate: boolean;
+  failed: boolean;
 
   constructor(private acteursProvider: ActeursProvider) { }
 
-  ngOnInit() {
-    this.acteur = new Acteur(null, null, null, null, null);
+  autoIncrement(): number {
+    let inc = 0;
+    this.acteurs.forEach( acteur => {
+       if (acteur.noAct > inc + 1) {
+         return inc + 1;
+       }
+       inc = acteur.noAct;
+    });
+    return inc + 1;
+  }
+
+  init() {
     this.acteursProvider.getAll().subscribe(acteurs => {
       this.acteurs = acteurs;
+      this.acteur = new Acteur(this.autoIncrement(), null, null, null, null);
     });
+  }
+
+  ngOnInit() {
+    this.init();
   }
 
   delete(id: any) {
-    this.acteursProvider.delete(id).subscribe( ret => {
-      console.log(ret);
-    });
+    this.acteursProvider.delete(id).subscribe(
+      () => {
+        this.validate = true;
+        this.failed = null;
+
+        console.log('Success');
+      },
+      () => {
+        this.validate = null;
+        this.failed = true;
+
+        console.log('Failed');
+      },
+      () => {
+        this.init();
+      }
+    );
   }
 
   add(acteur: Acteur) {
-    this.acteursProvider.add(acteur).subscribe( ret => {
-      console.log(ret);
-    });
+    this.acteursProvider.add(acteur).subscribe(
+      () => {
+        this.validate = true;
+        this.failed = null;
+
+        console.log('Success');
+      },
+      () => {
+        this.validate = null;
+        this.failed = true;
+
+        console.log('Failed');
+      },
+      () => {
+        this.init();
+      }
+    );
   }
 
 }
--- CinemaWebClient/src/app/models/acteur.ts
+++ CinemaWebClient/src/app/models/acteur.ts
@@ -2,6 +2,6 @@ export class Acteur {
   constructor(public noAct: number,
               public nomAct: string,
               public prenAct: string,
-              public dateNaiss: string,
-              public dateDeces: string) { }
+              public dateNaiss: Date,
+              public dateDeces: Date) { }
 }
